05c9408d4270174e3a9e522df62f6b396d745faa
Dastpa <patrickdastey@gmail.com>
2022-06-30 16:19:56 +0100

    Complete exercises 5, 6 & 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    
+    if ( !(typeof x === "number")
+      || !(typeof y === "number")
+      || x < 0 || y < 0 ) 
+      return("ERROR");
 
+    let total = x;
+
+    if (x < y) {
+        while (x !== y) {
+            x++;
+            total+= x;
+        }
+    } else if (x > y) {
+        while (x !== y) {
+            x--;
+            total+= x;
+        }
+    } else {
+        total = 0;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  if (typeof f !== "number") return("ERROR");
+  return parseFloat(((f - 32) * 5/9).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  if (typeof c !== "number") return("ERROR");
+  return parseFloat(((c * 9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
