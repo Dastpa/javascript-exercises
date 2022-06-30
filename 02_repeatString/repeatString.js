01cf7c15b59cd8098e6045f1628987c81f8768a4
Dastpa <patrickdastey@gmail.com>
2022-06-30 15:13:59 +0100

    Complete exercises 1 & 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, x) {
+    if(x < 0) return("ERROR");
 
+    let output = "";
+    for (let i = 0; i < x; i++) {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
