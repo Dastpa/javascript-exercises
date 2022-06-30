9957a5afdffa146f7877a7f7e62d9a3c478fe716
Dastpa <patrickdastey@gmail.com>
2022-06-30 15:41:02 +0100

    Complete exercises 3 & 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split("");
+    let arrRev = arr.reverse();
+    return arrRev.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...removeNums) {
+    return arr.filter( el => !removeNums.includes( el ) );
 };
 
 // Do not edit below this line
