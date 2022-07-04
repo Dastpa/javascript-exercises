6090eca4d6b3b4b95deb3470fdd5efb0ca4b4f82
Dastpa <patrickdastey@gmail.com>
2022-07-04 14:29:36 +0100

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = (people) => {
+    people.forEach(person => {
+        if (person.yearOfDeath === undefined) {person.yearOfDeath = 2022}
+    })
+    const ordered = people.sort((a,b) => (a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth) ? 1 : -1);
+    return ordered[0];
 };
 
 // Do not edit below this line
