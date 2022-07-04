d986f6c1453feb13ca1b1404f860e1da9cb05f50
Dastpa <patrickdastey@gmail.com>
2022-07-04 13:49:47 +0100

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = (string) => {
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    string = string.replace(/\s/g, "")
+    string = string.toLowerCase();
 
+    return reverseString(string) === string;
+};
+
+const reverseString = (string) => {
+    let arr = string.split("");
+    let revArr = arr.reverse();
+    let revStr = revArr.join("");
+
+    return revStr;
 };
 
 // Do not edit below this line
