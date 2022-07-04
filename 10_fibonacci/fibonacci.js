7bded66b5752cc1e7611db2889273612b6fd90f3
Dastpa <patrickdastey@gmail.com>
2022-07-04 13:58:19 +0100

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = (num) => {
+    num = parseInt(num);
+    if (num < 0) return 'OOPS'
 
+    let counter = 1;
+    let firstNum = 1;
+    let secondNum = 1;
+    let temp = 0;
+
+    while (counter !==  num) {
+        temp = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = temp;
+        counter++;
+    }
+
+    return firstNum;
 };
 
 // Do not edit below this line
